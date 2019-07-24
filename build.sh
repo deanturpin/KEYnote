62cdc4da7ab8ed785d65db6208a76654cc1d7d03
Definition of Dean <defofdean@gmail.com>
2019-07-24 13:26:46 +0100

    Fetch all projects, apply patches, build and run

--- build.sh
+++ build.sh
@@ -1,13 +1,57 @@
 #!/bin/bash
 
-zips=(
-OpenGLAppTutorial.zip
-SimpleFFTTutorial.zip
-SpectrumAnalyserTutorial.zip
+projects=(
+# DSP
+SimpleFFTTutorial
+SpectrumAnalyserTutorial
+
+# Graphics
+AnimationTutorial
+MainComponentTutorial
+GraphicsTutorial
+OpenGLAppTutorial
 )
 
-for zip in ${zips[@]}; do
-  curl --output $zip https://docs.juce.com/tutorials/ZIPs/$zip
-  unzip $zip
-  rm -f $zip
+# Start with a clean build area
+# rm -rf ${projects[@]}
+
+# Fetch all projects
+for project in ${projects[@]}; do
+
+  # Fetch the project if it doesn't exist
+  if [[ ! -d $project ]]; then
+    curl --output $project.zip https://docs.juce.com/tutorials/ZIPs/$project.zip
+    unzip $project.zip
+    rm -f $project.zip
+  fi
+done
+
+echo Applying patches
+sed -i s/SimpleFFTTutorial_01.h/SimpleFFTTutorial_02.h/ SimpleFFTTutorial/Source/Main.cpp
+sed -i s/AnimationTutorial_01.h/AnimationTutorial_04.h/ AnimationTutorial/Source/Main.cpp
+sed -i s/SpectrumAnalyserTutorial_01.h/SpectrumAnalyserTutorial_02.h/ SpectrumAnalyserTutorial/Source/Main.cpp
+
+# Build all projects
+for project in ${projects[@]}; do
+
+  build_dir=$project/Builds/LinuxMakefile
+
+  # Build for Linux if build area exists
+  if [[ -d $build_dir ]]; then
+    make -B --directory $build_dir -j $(nproc)
+  else
+    echo $build_dir does not exist
+  fi
+
+done
+
+# Run each project if there's an executable
+for project in ${projects[@]}; do
+
+  exe=$project/Builds/LinuxMakefile/build/$project
+
+  if [[ -f $exe ]]; then
+    $exe
+  fi
+
 done
