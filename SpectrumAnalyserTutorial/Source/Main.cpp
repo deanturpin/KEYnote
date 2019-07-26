e49aec4f671fb030de695b03661f3119419e6d6d
Definition of Dean <defofdean@gmail.com>
2019-07-26 09:34:06 +0100

    Set title

--- SpectrumAnalyserTutorial/Source/Main.cpp
+++ SpectrumAnalyserTutorial/Source/Main.cpp
@@ -20,7 +20,7 @@ public:
   const String getApplicationVersion() override { return "1.0.0"; }
 
   void initialise(const String &) override {
-    mainWindow.reset(new MainWindow("SpectrumAnalyserTutorial",
+    mainWindow.reset(new MainWindow("7th Sense Demo",
                                     new AnalyserComponent(), *this));
   }
   void shutdown() override { mainWindow = nullptr; }
