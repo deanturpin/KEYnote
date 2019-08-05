9dc5d84a2dc14aecebb324adbeb56c41163d6628
Definition of Dean <defofdean@gmail.com>
2019-08-05 14:46:54 +0100

    Trial yellow text

--- src/Source/SpectrumAnalyserTutorial_02.h
+++ src/Source/SpectrumAnalyserTutorial_02.h
@@ -205,7 +205,7 @@ public:
 
     // Display the calculated frequency and note
     g.setFont(60);
-    g.setColour(Colours::blue);
+    g.setColour(Colours::yellow);
     g.drawText(peak_note, getLocalBounds(), Justification::centred, true);
   }
 
